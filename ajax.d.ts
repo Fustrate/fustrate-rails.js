ce9b3547d8df0dc7f0e331d78f60033389242bb1
Steven Hoffman <git@fustrate.com>
2020-05-25 12:00:05 -0700

    make getCurrentPageJson use a generic type
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- ajax.d.ts
+++ ajax.d.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosPromise } from 'axios';
 
 export function when<T>(...requests: any[]): Promise<T>;
-export function getCurrentPageJson(): AxiosPromise;
+export function getCurrentPageJson<T = any>(): AxiosPromise<T>;
 
 export default Axios;
