991cabbfa05aaba08480861b4d93d4bc66aac85f
Steven Hoffman <git@fustrate.com>
2020-10-19 17:09:28 -0700

    not sure what the right type is
    Argument of type 'ShowArchitectural' is not assignable to parameter of type 'typeof GenericPage'.
      Property 'prototype' is missing in type 'ShowArchitectural' but required in type 'typeof GenericPage'.
    
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- decorators.d.ts
+++ decorators.d.ts
@@ -2,6 +2,6 @@ import type { GenericPage } from './index';
 
 type DecoratorTypes = 'autorefresh';
 
-export function autorefresh(): (target: typeof GenericPage, propertyKey: string, descriptor: PropertyDescriptor) => void;
+export function autorefresh(): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => void;
 
 export function callDecoratedMethods(instance: GenericPage, type: DecoratorTypes): void;
