fbf6689c74fb84235e9154f3c3ad1d81f0b48c28
Steven Hoffman <git@fustrate.com>
2021-10-26 20:17:51 -0700

    correct ajax default export
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- ajax.d.ts
+++ ajax.d.ts
@@ -1,6 +1,6 @@
-import Axios, { AxiosPromise } from 'axios';
+import AxiosInstance, { AxiosPromise } from 'axios';
 
 export function when<T>(...requests: any[]): Promise<T>;
 export function getCurrentPageJson<T = any>(): AxiosPromise<T>;
 
-export default Axios;
+export default AxiosInstance;
