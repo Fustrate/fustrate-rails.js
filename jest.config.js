b4bb137bd3d95d6aebd53541ed50532d21152a9f
Steven Hoffman <fustrate@me.com>
2022-09-12 15:55:25 -0700

    include polyfills before testing
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
+require('./src/polyfills');
+
 module.exports = {
   moduleFileExtensions: ['js', 'json'],
   roots: ['./src/', './test/'],
