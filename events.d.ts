f9cea02ae7f3214f037b30a9ed9afa78e57c80f1
Steven Hoffman <git@fustrate.com>
2023-10-11 10:48:01 -0700

    open up typing

--- events.d.ts
+++ events.d.ts
@@ -6,5 +6,6 @@ interface MatchesOptions {
 }
 
 export function delegate<K extends keyof HTMLElementEventMap>(element: HTMLElement, selector: string | MatchesOptions, eventType: K, handler: (ev: HTMLElementEventMap[K]) => any): void;
+export function delegate(element: HTMLElement, selector: string | MatchesOptions, eventType: string, handler: EventListenerOrEventListenerObject): void;
 export function fire<T = any>(target: EventTarget | Listenable, name: string, detail?: T): boolean;
 export function stopEverything(event: Event): void;
--- listenable.d.ts
+++ listenable.d.ts
@@ -1,7 +1,7 @@
 export default class Listenable {
   protected listeners: { [s: string]: ((event: CustomEvent) => void)[] };
 
-  public addEventListener(type: string, listener: (event: CustomEvent) => void): void;
+  public addEventListener<T = CustomEvent>(type: string, listener: (event: T) => void): void;
   public removeEventListener(type: string, listener: (event: CustomEvent) => void): void;
 
   public dispatchEvent(event: CustomEvent): boolean;
