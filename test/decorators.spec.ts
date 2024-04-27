5de0a4fc58c904c96237510d747799c86048a2d6
Steven Hoffman <fustrate@me.com>
2024-04-27 09:28:12 -0700

    add tests for decorators
    Signed-off-by: Steven Hoffman <fustrate@me.com>

---
+++ test/decorators.spec.ts
@@ -0,0 +1,82 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { decorateMethod, callDecoratedMethods } from '../decorators';
+
+const simple = decorateMethod('$simple');
+
+const oneArg = decorateMethod('$oneArg');
+
+const manyArgs = decorateMethod('$manyArgs');
+
+const dynamic = (name: string) => decorateMethod(name);
+
+class BaseClass {
+  public callDecoratedMethods() {
+    return callDecoratedMethods(this, '$simple');
+  }
+
+  public callDecoratedMethodsWithOneArg(arg: number) {
+    return callDecoratedMethods(this, '$oneArg', arg)
+  }
+
+  public callDecoratedMethodsWithManyArgs(one: number, two: string, three: Record<string, number>) {
+    return callDecoratedMethods(this, '$manyArgs', one, two, three)
+  }
+
+  public callDynamicDecorator(name: string, ...args: any[]) {
+    return callDecoratedMethods(this, name, ...args);
+  }
+}
+
+class Subclass extends BaseClass {
+  @simple
+  protected aSimpleMethod() {
+    return 1;
+  }
+
+  @simple
+  protected anotherSimpleMethod() {
+    return 11;
+  }
+
+  @simple
+  protected oneMoreSimpleMethod() {
+    return 111;
+  }
+
+  @oneArg
+  protected oneArgMethod(arg: number) {
+    return arg + 1;
+  }
+
+  @oneArg
+  protected anotherOneArgMethod(arg: number) {
+    return arg - 8;
+  }
+
+  @manyArgs
+  protected manyArgsMethod(one: number, two: string, three: Record<string, number>) {
+    return `${two}: ${one + Object.values(three).reduce((total, num) => total + num)}`;
+  }
+
+  @dynamic('potato')
+  protected makeFries(sentence: string) {
+    return sentence.split(' ');
+  }
+}
+
+describe('decorateMethod', () => {
+  it('joins words and stuff', () => {
+    const record = new Subclass();
+
+    expect(record.callDecoratedMethods()).toStrictEqual([1, 11, 111]);
+
+    expect(record.callDecoratedMethodsWithOneArg(7)).toStrictEqual([8, -1]);
+
+    expect(record.callDecoratedMethodsWithManyArgs(3, 'total', { five: 5, eleven: 11 }))
+      .toStrictEqual(['total: 19']);
+
+    expect(record.callDynamicDecorator('potato', 'porpoise purpose'))
+      .toStrictEqual([['porpoise', 'purpose']]);
+  });
+});
