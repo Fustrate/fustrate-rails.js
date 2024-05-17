d78caa0e78ffdbb2dcac1bf54888657c3440cf9b
Steven Hoffman <git@fustrate.com>
2024-05-17 10:34:46 -0700

    allow setting options in initializer

--- components/dropdown.ts
+++ components/dropdown.ts
@@ -1,28 +1,40 @@
-import { createPopper, type Instance } from '@popperjs/core';
+import { createPopper, type Instance, type Options, type Placement } from '@popperjs/core';
 
-import { delegate } from '../events';
+import { delegate, stopEverything } from '../events';
 
 let popper: Instance | undefined;
 let boundHide: any;
+
+let defaultOptions: Partial<Options>;
 
 function open(event: UIEvent & { target: HTMLElement }): false {
   // Hide any visible dropdowns before showing this one
   hide();
 
-  popper = createPopper(event.target, event.target.nextElementSibling as HTMLElement, {
-    placement: 'bottom-start',
-    modifiers: [
-      {
-        name: 'flip',
-        options: {
-          behavior: ['bottom', 'top'],
-        },
+  const options = {
+    modifiers: [],
+    ...defaultOptions,
+  };
+
+  if (event.target.dataset.popperPlacement) {
+    options.placement = event.target.dataset.popperPlacement as Placement;
+  }
+
+  if (event.target.dataset.popperFlip) {
+    options.modifiers.push({
+      name: 'flip',
+      options: {
+        behavior: ['bottom', 'top'],
       },
-    ],
-  });
+    });
+  }
+
+  popper = createPopper(event.target, event.target.nextElementSibling as HTMLElement, options);
 
   // The next time we click something on the page, hide the dropdown
   document.body.addEventListener('click', boundHide);
+
+  stopEverything(event);
 
   return false;
 }
@@ -33,10 +45,10 @@ function hide(): void {
   document.body.removeEventListener('click', boundHide);
 }
 
-export default {
-  initialize: () => {
-    delegate(document.body, '.has-dropdown', 'click', open.bind(this));
+export function initialize(options?: Partial<Options>) {
+  defaultOptions = options ?? {};
 
-    boundHide = hide.bind(this);
-  },
-};
+  delegate(document.body, '.has-dropdown', 'click', open);
+
+  boundHide = hide;
+}
