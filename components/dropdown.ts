303f5fd676d2c2f7a4556415c73de49e0bc6bc75
Steven Hoffman <git@fustrate.com>
2024-05-17 10:24:32 -0700

    change how poppers are initialized

--- components/dropdown.ts
+++ components/dropdown.ts
@@ -1,42 +1,42 @@
 import { createPopper, type Instance } from '@popperjs/core';
 
 import { delegate } from '../events';
-import Listenable from '../listenable';
 
-export default class Dropdown extends Listenable {
-  private static popper?: Instance;
-  private static boundHide: any;
+let popper: Instance | undefined;
+let boundHide: any;
 
-  public static initialize(): void {
-    delegate(document.body, '.has-dropdown', 'click', this.open.bind(this));
+function open(event: UIEvent & { target: HTMLElement }): false {
+  // Hide any visible dropdowns before showing this one
+  hide();
 
-    this.boundHide = this.hide.bind(this);
-  }
-
-  protected static open(event: UIEvent & { target: HTMLElement }): false {
-    // Hide any visible dropdowns before showing this one
-    this.hide();
-
-    this.popper = createPopper(event.target, event.target.nextElementSibling as HTMLElement, {
-      placement: 'bottom-start',
-      modifiers: [
-        {
-          name: 'flip',
-          options: {
-            behavior: ['bottom', 'top'],
-          },
+  popper = createPopper(event.target, event.target.nextElementSibling as HTMLElement, {
+    placement: 'bottom-start',
+    modifiers: [
+      {
+        name: 'flip',
+        options: {
+          behavior: ['bottom', 'top'],
         },
-      ],
-    });
+      },
+    ],
+  });
 
-    document.body.addEventListener('click', this.boundHide);
+  // The next time we click something on the page, hide the dropdown
+  document.body.addEventListener('click', boundHide);
 
-    return false;
-  }
-
-  protected static hide(): void {
-    this.popper?.destroy();
-
-    document.body.removeEventListener('click', this.boundHide);
-  }
+  return false;
 }
+
+function hide(): void {
+  popper?.destroy();
+
+  document.body.removeEventListener('click', boundHide);
+}
+
+export default {
+  initialize: () => {
+    delegate(document.body, '.has-dropdown', 'click', open.bind(this));
+
+    boundHide = hide.bind(this);
+  },
+};
