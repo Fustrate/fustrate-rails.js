26392aa4ccbb2e16279ee00803f01cb31c112ea5
Steven Hoffman <git@fustrate.com>
2025-03-05 15:06:46 -0800

    allow setting a custom ajax response error handler

--- ajax.ts
+++ ajax.ts
@@ -1,13 +1,14 @@
 import axios, { type AxiosResponse } from 'axios';
 
-import Flash from './components/flash';
 import { addDebugData } from './debug';
 
 export function csrfToken(): string | undefined {
   return document.querySelector<HTMLMetaElement>('meta[name=csrf-token]')?.content;
 }
 
-function processResponseError(response: AxiosResponse<{ errors?: string[] }>) {
+type ResponseErrorHandler = (response: AxiosResponse<{ errors?: string[] }>) => void;
+
+let onResponseError: ResponseErrorHandler = (response: AxiosResponse<{ errors?: string[] }>) => {
   const { data, status } = response;
 
   if (!status || status === 404) {
@@ -19,13 +20,15 @@ function processResponseError(response: AxiosResponse<{ errors?: string[] }>) {
       'You are not currently logged in. Please refresh the page and try performing this action again. To prevent this in the future, check the "Remember Me" box when logging in.',
     );
   } else if (data.errors) {
-    for (const message of data.errors) {
-      Flash.error(message);
-    }
+    console.log('Errors encountered:', data.errors);
   } else {
     // eslint-disable-next-line no-console
     console.log(`Unhandled interception (${status})`, response);
   }
+};
+
+export function setResponseErrorHandler(f: ResponseErrorHandler): void {
+  onResponseError = f;
 }
 
 function processRequestError(request: Record<string, any>) {
@@ -60,12 +63,12 @@ instance.interceptors.response.use(
   (response) => response,
   (error) => {
     if ('response' in error && error.response) {
-      processResponseError(error.response);
+      onResponseError(error.response);
     } else if ('request' in error && error.request) {
       processRequestError(error.request);
     }
 
-    return Promise.reject(new Error(error));
+    return Promise.reject(error);
   },
 );
 
