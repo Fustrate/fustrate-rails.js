bf9349c3a2124dbf89eda6c52d042572f0464ae1
Steven Hoffman <fustrate@me.com>
2026-03-30 13:17:22 -0700

    restore setResponseErrorHandler

--- ajax.ts
+++ ajax.ts
@@ -1,4 +1,4 @@
-import ky, { type BeforeErrorHook } from 'ky';
+import ky, { type KyRequest, type BeforeErrorHook, type KyResponse } from 'ky';
 
 import { addDebugData } from './debug';
 
@@ -6,25 +6,49 @@ export function csrfToken(): string | undefined {
   return document.querySelector<HTMLMetaElement>('meta[name=csrf-token]')?.content;
 }
 
-const onResponseError: BeforeErrorHook = async (error) => {
-  const { request, response } = error;
-  const { url, method } = request;
-  const { status, statusText } = response;
+type ResponseErrorHandler = (response: KyResponse<{ errors?: string[] }>) => void;
 
-  addDebugData({ status, url, method, statusText });
+let onResponseError: ResponseErrorHandler = (response: KyResponse<{ errors?: string[] }>) => {
+  const { status } = response;
 
   if (!status || status === 404) {
     window.alert('The server is not responding - please wait a while before trying again.');
   } else if (status === 401) {
     window.alert(
-      'You are not currently logged in. Please refresh the page and try performing this action again. ' +
-        'To prevent this in the future, check the "Remember Me" box when logging in.',
+      'You are not currently logged in. Please refresh the page and try performing this action again. To prevent this in the future, check the "Remember Me" box when logging in.',
     );
   } else {
-    console.log(`Unhandled interception (${status})`, error.response);
+    response.json().then((data) => {
+      if (data.errors) {
+        console.log('Errors encountered:', data.errors);
+      } else {
+        console.log(`Unhandled interception (${status})`, response);
+      }
+    });
+  }
+};
+
+export function setResponseErrorHandler(f: ResponseErrorHandler): void {
+  onResponseError = f;
+}
+
+function processRequestError(request: KyRequest) {
+  window.alert('There was a problem connecting to the server - please wait a while before trying again.');
+
+  addDebugData({
+    destination: request.destination,
+  });
+}
+
+const errorHandler: BeforeErrorHook = async (error) => {
+  error.response?.status;
+  if ('response' in error && error.response) {
+    onResponseError(error.response);
+  } else if ('request' in error && error.request) {
+    processRequestError(error.request);
   }
 
-  return error;
+  return Promise.reject(error);
 };
 
 const instance = ky.create({
@@ -32,15 +56,18 @@ const instance = ky.create({
     'X-CSRF-Token': csrfToken(),
   },
   hooks: {
-    beforeError: [onResponseError],
+    beforeError: [errorHandler],
   },
-  // stringifyJson: data => JSON.stringify(data, (key, value) => {
-  //   if (key.endsWith('_at')) {
-  //       return DateTime.fromISO(value);
-  //   }
-
-  //   return value;
-  // }),
 });
+
+// export async function when<T>(...requests: any[]): Promise<T> {
+//   return new Promise((resolve) => {
+//     axios.all(requests).then(
+//       axios.spread((...responses) => {
+//         resolve(responses as T);
+//       }),
+//     );
+//   });
+// }
 
 export default instance;
