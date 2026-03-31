a1b7fff5822b3b8057706395e03b26af24d83b1f
Steven Hoffman <fustrate@me.com>
2026-03-31 10:42:54 -0700

    make json functions actually return the JSON
    if we need the raw response, we can use ./ajax instead

--- json.ts
+++ json.ts
@@ -1,5 +1,3 @@
-import type { KyResponse } from 'ky';
-
 import ajax from './ajax';
 import type { PaginatedData } from './components/pagination';
 
@@ -21,22 +19,26 @@ function pathToJsonURL(path: string): string {
   return url.toString();
 }
 
-export async function getJSON<T>(url: string): Promise<KyResponse<T>> {
-  return ajax.get<T>(pathToJsonURL(url));
+export async function getJSON<T>(url: string): Promise<T> {
+  const response = ajax.get<T>(pathToJsonURL(url), { headers: { 'content-type': 'application/json' } });
+
+  return await response.json();
 }
 
-export async function patchJSON<T>(url: string, data: any): Promise<KyResponse<T>> {
-  return ajax.patch<T>(pathToJsonURL(url), { body: data });
+export async function patchJSON<T>(url: string, data: any): Promise<T> {
+  const response = ajax.patch<T>(pathToJsonURL(url), { json: data, headers: { 'content-type': 'application/json' } });
+  return await response.json();
 }
 
-export async function postJSON<T>(url: string, data?: any): Promise<KyResponse<T>> {
-  return ajax.post<T>(pathToJsonURL(url), { body: data });
+export async function postJSON<T>(url: string, data?: any): Promise<T> {
+  const response = ajax.post<T>(pathToJsonURL(url), { json: data, headers: { 'content-type': 'application/json' } });
+  return await response.json();
 }
 
-export async function getCurrentPageJSON<T = any>(): Promise<KyResponse<T>> {
+export async function getCurrentPageJSON<T = any>(): Promise<T> {
   return getJSON<T>(window.location.toString());
 }
 
-export async function getPaginatedJSON<T>(url: string): Promise<KyResponse<PaginatedData<T>>> {
+export async function getPaginatedJSON<T>(url: string): Promise<PaginatedData<T>> {
   return getJSON<PaginatedData<T>>(url);
 }
